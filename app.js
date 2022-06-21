8e0d0e8e02fe2b713896b125d168aabebd1984f9
Enrique Saracho Felix <esarachof97@gmail.com>
2022-06-21 10:59:43 -0600

    Update app.js
    Updated app.js file, on first load displays random quote's author, User Story 7

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const text = document.getElementById("text");
+  const author = document.getElementById("author");
 
-  const quotes = ["first quote", "second quote", "third quote"];
+  const quotes = [
+    { quote: "first quote", author: "first author" },
+    { quote: "second quote", author: "first author" },
+    { quote: "third quote", author: "second author" },
+  ];
 
-  text.textContent = quotes[Math.floor(Math.random() * quotes.length)];
+  randomNumber = Math.floor(Math.random() * quotes.length);
+  text.textContent = quotes[randomNumber].quote;
+  author.textContent = quotes[randomNumber].author;
 });
